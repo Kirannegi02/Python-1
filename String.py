3abafd352d2e6c621e8462ad9c20c9539769cee7
Kiran <negiaaradhya02@gmail.com>
2022-02-11 21:23:12 +0530

    Python file is created

---
+++ String.py
@@ -0,0 +1,32 @@
+#String methods
+
+# Upper Case
+
+a = "Hello, World !"
+print(a.upper())
+
+# Lower Case
+a = "Hello, World !"
+print(a.lower())
+
+# Remove Whitespace   strip() method removes any whitespace from beginning or the end.
+
+a = "Hello,  World !"
+print(a.strip())
+
+# Replace string    replace() method replaces a string with another string.
+
+a = "Hello,  World !"
+print(a. replace("H","J"))
+
+# Split String
+
+a = "Hello,  World !"
+print(a. split(","))
+
+# String concatenation
+
+a = "Hello"
+b = "World"
+c = a+b
+print(c)
